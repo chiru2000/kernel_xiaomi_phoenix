69eb84a94006f558f548f13de0185f8db422df5d
Sultan Alsawaf <sultan@kerneltoast.com>
2018-12-09 17:57:50 -0800

    msm: kgsl: Wake GPU upon receiving an ioctl rather than upon touch input
    Waking the GPU upon touch wastes power when the screen is being touched
    in a way that does not induce animation or any actual need for GPU usage.
    Instead of preemptively waking the GPU on touch input, wake it up upon
    receiving a IOCTL_KGSL_GPU_COMMAND ioctl since it is a sign that the GPU
    will soon be needed.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>
    Signed-off-by: Udit Karode <udit.karode@gmail.com>
    
     Conflicts:
    	drivers/gpu/msm/kgsl_device.h
    
    Signed-off-by: Udit Karode <udit.karode@gmail.com>
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -60,6 +60,7 @@ MODULE_PARM_DESC(swfdetect, "Enable soft fault detection");
 
 #define KGSL_LOG_LEVEL_DEFAULT 0
 
+static void adreno_pwr_on_work(struct work_struct *work);
 static unsigned int counter_delta(struct kgsl_device *device,
 	unsigned int reg, unsigned int *counter);
 
@@ -113,6 +114,8 @@ static struct adreno_device device_3d0 = {
 		.skipsaverestore = 1,
 		.usesgmem = 1,
 	},
+	.pwr_on_work = __WORK_INITIALIZER(device_3d0.pwr_on_work,
+		adreno_pwr_on_work),
 };
 
 /* Ptr to array for the current set of fault detect registers */
@@ -361,6 +364,19 @@ void adreno_fault_detect_stop(struct adreno_device *adreno_dev)
 	}
 
 	adreno_dev->fast_hang_detect = 0;
+}
+
+static void adreno_pwr_on_work(struct work_struct *work)
+{
+	struct adreno_device *adreno_dev =
+		container_of(work, typeof(*adreno_dev), pwr_on_work);
+	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
+
+	mutex_lock(&device->mutex);
+
+	kgsl_pwrctrl_change_state(device, KGSL_STATE_ACTIVE);
+
+	mutex_unlock(&device->mutex);
 }
 
 /*
@@ -1014,9 +1030,6 @@ static int adreno_of_get_power(struct adreno_device *adreno_dev,
 	device->pwrctrl.bus_control = of_property_read_bool(node,
 		"qcom,bus-control");
 
-	device->pwrctrl.input_disable = of_property_read_bool(node,
-		"qcom,disable-wake-on-touch");
-
 	return 0;
 }
 
@@ -1313,6 +1326,7 @@ static int adreno_probe(struct platform_device *pdev)
 		KGSL_DRV_WARN(device,
 			"Failed to get gpuhtw LLC slice descriptor %ld\n",
 			PTR_ERR(adreno_dev->gpuhtw_llc_slice));
+
 out:
 	if (status) {
 		adreno_ringbuffer_close(adreno_dev);
